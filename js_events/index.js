ad73f735741fbfc03b14c984e8959f92b766b451
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-06-27 23:32:10 +0200

    js_events try to make a mouse move that moves an element

--- js_events/index.js
+++ js_events/index.js
@@ -17,5 +17,23 @@ function loadChildCount(event) {
     event.stopPropagation() //Stop propagation of the event to parents of the element in the same zone of the event location. --> The click on the child, doesn't create an event click on its parent.
 }
 
+parent.addEventListener('mousemove', function (event) {
+    child = document.getElementById("child")
+    if (clicked === true) {
+        child.style.top = event.offsetY + "px"
+        child.style.left = event.offsetX + "px"
+    }
+
+});
+var clicked = true
+parent.addEventListener('mousedown', function (event) {
+    clicked = true
+    console.log("clicked true")
+});
+
+parent.addEventListener('mouseup', function (event) {
+    clicked = false
+    console.log("clicked false")
+});
 
 
