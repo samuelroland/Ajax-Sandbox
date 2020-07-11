c4b15f39b278acc2a1f309fa9751973bc6414d4b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-07-11 11:08:44 +0200

    get_meteo added image of current condition

--- get_meteo/index.js
+++ get_meteo/index.js
@@ -18,12 +18,22 @@ function askWeather() {
     * and in HTML for a button for ex, that execute the function "test()" when the event click start:
     * <button id="btnTest" onclick="test()" />
     * */
-
+    res = document.getElementById("weather-result")
+    divImg = document.getElementById("img-result")
+    divImg.innerHTML = ""
+    res.innerText = ""
     request.onreadystatechange = function () {
         //write "this" is equal to write "event.target"
         if (this.readyState == XMLHttpRequest.DONE && this.status == 200) { //if the request is DONE so the state is 4. and if the request is success.
             var response = JSON.parse(this.responseText);   //parse in JSOn the response of the request
             console.log(response.current_condition.condition);  //display one field of the data received
+
+            //Display the text about the current condition
+            res.innerText = response.current_condition.condition
+
+            //Create an image with the current weather
+            divImg.appendChild(document.createElement("img"))
+            divImg.firstChild.src = response.current_condition.icon
         }
     };
     request.open("GET", "https://www.prevision-meteo.ch/services/json/paris")   //prepare the request with method and url
