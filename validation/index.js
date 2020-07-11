1100c2a9cf48951b5a791fe5bc005dee22e1b9df
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-07-11 17:49:48 +0200

    validation: finished js and html

--- validation/index.js
+++ validation/index.js
@@ -2,3 +2,24 @@
  * CodePen Source: https://codepen.io/nicolaspatschkowski/pen/GRJQvGY
  * Author: Samuel Roland
  */
+
+//Variables and events declarations
+codeValid = document.getElementById("code-validation")
+code.addEventListener("input", checkCode)
+btnSubmit = document.getElementById("submit-btn")
+
+//INFO: regex in js start and finish with "/" but not in HTML in the attribut pattern !
+//Ex: in JS--> /^CODE-/test(code.value) and in HTML--> pattern="^CODE-"
+
+//Check validity of the code value
+function checkCode() {
+    if (/^CODE-/.test(code.value) == true) {    //regex: if start with "CODE-" value it's valid
+        codeValid.innerText = "Code valide"
+        btnSubmit.removeAttribute("disabled")
+    } else {    //else display invalid and disable the submit button
+        codeValid.innerText = "Code invalide"
+        btnSubmit.setAttribute("disabled", true)
+    }
+}
+
+checkCode()
