524c09dde0452673cd58a952fafc445aae77df74
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-07-12 12:27:43 +0200

    async_code: import files

---
+++ async_code/index.js
@@ -0,0 +1,12 @@
+/*
+ * CodePen Source: https://codepen.io/nicolaspatschkowski/pen/NWqyaKP
+ * Author: Samuel Roland
+ */
+
+async function getNumber1() {
+    return 10;
+}
+
+async function getNumber2() {
+    return 4;
+}
