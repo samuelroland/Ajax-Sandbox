9d1febeb330873b7e02397057c2f93debd0e6449
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2020-07-12 12:47:56 +0200

    async_code finished
    very short and not in real condition...

--- async_code/index.js
+++ async_code/index.js
@@ -10,3 +10,15 @@ async function getNumber1() {
 async function getNumber2() {
     return 4;
 }
+
+//Function to create: compute()
+async function compute() {
+    val1 = await getNumber1()
+    val2 = await getNumber2()
+    return val1 + val2
+}
+
+compute()
+    .then(function (total) {
+        result.innerText = total
+    })
